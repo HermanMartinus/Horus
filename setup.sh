9288b30e5fdce8c32aefcac651fb7792174de6ea
Herman Martinus <hfbmartinus@gmail.com>
2018-10-26 15:54:29 +0200

    Changed to `.pwd`

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 chmod +x horus.py
 cp horus.py horus
-mkdir -p ~/pwd
-mv horus ~/pwd/
-cp authenticator.json ~/pwd/
-export PATH=$PATH":$HOME/pwd"
+mkdir -p ~/.pwd
+mv horus ~/.pwd/
+cp authenticator.json ~/.pwd/
+export PATH=$PATH":$HOME/.pwd"
